3c93f6dac411b70a36225bf06fdf2cf59ed3736c
mariospecht <mariospecht@10.41.203.176>
2018-10-17 23:02:20 -0300

    Implementado o metodo ls

--- T2_RMI/src/br/pucrs/rmi/model/Client.java
+++ T2_RMI/src/br/pucrs/rmi/model/Client.java
@@ -9,7 +9,7 @@ public class Client {
 	public static void main (String[] argv) {
 		try {
 			FSInterface hello = (FSInterface) Naming.lookup ("//localhost/Client");
-			System.out.println (hello.mkdir("TESTE"));
+			System.out.println (hello.ls(""));
 		} catch (Exception e) {
 			System.out.println ("Client failed:");
 			e.printStackTrace();
